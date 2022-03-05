0b5603cd5bab86fb7d6f44c0318da6b400a0843c
Dima <gaponoffd@yandex.ru>
2022-03-05 11:46:25 +0300

    react-color-palette realization

--- src/components/TextBlock.js
+++ src/components/TextBlock.js
@@ -1,15 +1,15 @@
-import React, {useContext} from 'react';
+import React, {useState, useContext} from 'react';
 import Context from '../context';
-import { MdFormatAlignLeft, MdFormatAlignJustify, MdFormatAlignRight, MdDeleteOutline } from "react-icons/md";
-
+import { MdFormatAlignLeft, MdFormatAlignJustify, MdFormatAlignRight, MdDeleteOutline, MdColorLens, MdBlurOn } from "react-icons/md";
+import { ColorPicker, useColor } from "react-color-palette";
+import "react-color-palette/lib/css/styles.css";
 
 export default function TextBlock ({block, deleteBlock}) {
-
   const {editsText, editStyle} = useContext(Context)
-  let [text, setText] = React.useState(block.content)
-
-  let [styles, setStyles] = React.useState(block.style)
-
+  const [text, setText] = useState(block.content);
+  const [styles, setStyles] = useState(block.style);
+  const [toggler, setToggler] = useState(false);
+  const [color, setColor] = useColor("hex", "#894040");
 
   function textDirection(direction) {
     let newObj = {}
@@ -19,20 +19,46 @@ export default function TextBlock ({block, deleteBlock}) {
     editStyle(block.id, newObj)
   }
 
+  function textColor(color) {
+    let newObj = {}
+    Object.assign(newObj, styles)
+    newObj.color = color.hex;
+    setColor(color);
+    setStyles(newObj);
+    editStyle(block.id, newObj);
+  }
+
   return (
     <div className="editor-block" id={block.id}>
+      {toggler ?  
+        <div className='colorpicker'>
+          <ColorPicker 
+            width={400}
+            height={200}
+            color={color}
+            hideHSV
+            dark
+            onChange={textColor}
+            />
+        </div>
+        : ""
+      }
       <div className="block-navbar" >
         <div className="position-block">
           <button onClick={()=> textDirection('left')}><MdFormatAlignLeft/></button>
           <button onClick={()=> textDirection('center')}><MdFormatAlignJustify/></button>
           <button onClick={()=> textDirection('right')}><MdFormatAlignRight/></button>
+          <button onClick={()=> setToggler(!toggler)}><MdColorLens/></button>
         </div>
-        <button className="delete-block" onClick={ () => deleteBlock(block.id) }><MdDeleteOutline /></button>
+        <div className="position-block">
+          <button className="delete-block" onClick={ () => deleteBlock(block.id) }><MdDeleteOutline /></button>
+          <button><MdBlurOn/></button>
+        </div>
       </div>
       <div className="place" >
         <textarea 
-          style={block.style} 
-          value={text} 
+          style={styles}
+          value={text}
           onChange={event =>{
             setText(event.target.value);
             editsText(block.id, event.target.value)
--- src/components/TitleBlock.js
+++ src/components/TitleBlock.js
@@ -1,15 +1,11 @@
-import React, {useState, useContext, useEffect} from 'react';
+import React, {useState, useContext} from 'react';
 import Context from '../context';
 import { MdFormatAlignLeft, MdFormatAlignJustify, MdFormatAlignRight, MdDeleteOutline, MdColorLens, MdBlurOn } from "react-icons/md";
-
-
 import { ColorPicker, useColor } from "react-color-palette";
 import "react-color-palette/lib/css/styles.css";
 
 export default function TitleBlock ({block, deleteBlock}) {
-
   const {editsText, editStyle} = useContext(Context)
-
   const [text, setText] = useState(block.content);
   const [styles, setStyles] = useState(block.style);
   const [toggler, setToggler] = useState(false);
@@ -23,41 +19,36 @@ export default function TitleBlock ({block, deleteBlock}) {
     editStyle(block.id, newObj)
   }
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  function textColor() {
+  function textColor(color) {
     let newObj = {}
     Object.assign(newObj, styles)
-    newObj.color = color;
+    newObj.color = color.hex;
+    setColor(color);
     setStyles(newObj);
     editStyle(block.id, newObj);
-    return false
   }
-
-  useEffect(() => {
-    setToggler(false);
-  }, [color])
-  
 
   return (
     <div className="editor-block" id={block.id}>
+      {toggler ?  
+        <div className='colorpicker'>
+          <ColorPicker 
+            width={400}
+            height={200}
+            color={color}
+            hideHSV
+            dark
+            onChange={textColor}
+            />
+        </div>
+        : ""
+      }
       <div className="block-navbar" >
         <div className="position-block">
           <button onClick={()=> textDirection('left')}><MdFormatAlignLeft/></button>
           <button onClick={()=> textDirection('center')}><MdFormatAlignJustify/></button>
           <button onClick={()=> textDirection('right')}><MdFormatAlignRight/></button>
           <button onClick={()=> setToggler(!toggler)}><MdColorLens/></button>
-          {toggler ?  
-            <div className='colorpicker'>
-              <ColorPicker 
-                width={400}
-                height={200}
-                color={color}
-                hideHSV
-                dark
-                onChange={setColor}/>
-            </div>
-            : ""
-          }
         </div>
         <div className="position-block">
           <button className="delete-block" onClick={ () => deleteBlock(block.id) }><MdDeleteOutline /></button>
